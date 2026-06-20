fed1e16d62440dd968565a0fd2ae3a648ca9b065
rwalas298-oss <rwalas298@gmail.com>
2026-06-20 13:14:34 -0500

    Update nsfw-pack.js
    Guardar

--- comandos/nsfw-pack.js
+++ comandos/nsfw-pack.js
@@ -1,69 +1,32 @@
-import axios from 'axios'
-
 const pack = {
     name: 'pack',
-    alias: ['girls'],
-    category: 'nsfw',
     noPrefix: true,
 
     run: async (conn, m) => {
         try {
             await conn.sendMessage(m.chat, {
-                react: {
-                    text: '📦',
-                    key: m.key
-                }
-            })
-
-            const { data } = await axios.get(
-                'https://api.delirius.store/nsfw/girls'
-            )
-
-            const image = data?.data?.url || data?.url
-
-            if (!image) {
-                return m.reply('❌ No se pudo obtener una imagen.')
-            }
-
-            await conn.sendMessage(
-                m.chat,
-                {
-                    image: {
-                        url: image
-                    },
-
-                    caption:
-                        '📦 *Pack Girls*\n\n' +
-                        'Presiona el botón para otra imagen aleatoria.',
-
-                    footer: 'SaitamaBot-Sckt-MD',
-
-                    buttons: [
-                        {
-                            name: 'quick_reply',
-                            buttonParamsJson: JSON.stringify({
-                                display_text: '➡️ Siguiente Imagen',
-                                id: '.pack'
-                            })
-                        }
-                    ]
+                image: {
+                    url: 'https://api.delirius.store/nsfw/girls'
                 },
-                {
-                    quoted: m
-                }
-            )
 
-        } catch (error) {
-            console.log(error)
+                caption: '📦 *Pack Girls*\n\n➡️ Pulsa para otra imagen aleatoria',
 
-            await conn.sendMessage(m.chat, {
-                react: {
-                    text: '❌',
-                    key: m.key
-                }
-            })
+                footer: 'Todleys Bot',
 
-            m.reply('❌ Error al obtener la imagen.')
+                buttons: [
+                    {
+                        name: 'quick_reply',
+                        buttonParamsJson: JSON.stringify({
+                            display_text: '➡️ Siguiente Pack',
+                            id: '.pack'
+                        })
+                    }
+                ]
+            }, { quoted: m })
+
+        } catch (e) {
+            console.log(e)
+            m.reply('❌ Error al obtener imagen')
         }
     }
 }
